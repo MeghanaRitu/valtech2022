dc69547e24432afc4ec687db7ecbf6144c6213af
Meghana R <Meghana.R@valtech.com>
2022-09-30 15:46:59 +0530

    ytytyty

--- Devops/src/Devops/Arithmetic.java
+++ Devops/src/Devops/Arithmetic.java
@@ -1,6 +1,9 @@
 package Devops;
-public interface Arithmetic {
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Arithmetic {
 public static void main(String[] args) {
-	
+	System.out.println("jhjhjhj");
 }
 }
